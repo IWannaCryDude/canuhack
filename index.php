1623b8a6d5f22d6f747b939892c497ea1aa2ce27
WannaCry <iwannacrydude@gmail.com>
2022-01-09 19:29:18 +0000

    Update index.php

--- index.php
+++ index.php
@@ -10,5 +10,16 @@
 
 <!DOCTYPE html>
 <html>
-
+    <head>
+        <title>Can U Hack?</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <h3>Introduce the password down here:</h3>
+        <form action="access.php" method="POST" id="form">
+            <input type="text" name="pass">
+            <input type="submit" value="Send" name="submit">
+            <p><a href="index.php">Go back</a>
+        </form>
+    </body>
 </html>
