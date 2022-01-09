0ef44f84919ef0b4a9cd9f3ae289da333f957f9c
WannaCry <iwannacrydude@gmail.com>
2022-01-09 19:27:24 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    // Connect to database
+    $conn = mysqli_connect('localhost', 'root', 'K1d_searxin0', 'canuhack');
+    //Check connection
+    if(!$conn) {
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+</html>
